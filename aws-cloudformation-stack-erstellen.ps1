08ee3a8dc75ecaef495d07ed3db9759cccbc199b
tidiane <tidian.balde@inform-software.com>
2023-10-29 16:42:15 +0100

    all changes made today was nice

--- aws-cloudformation-stack-erstellen.ps1
+++ aws-cloudformation-stack-erstellen.ps1
@@ -1,1 +1,2 @@
+#Das Präfix "file://" ist eine Konvention, um anzuzeigen, dass der nachfolgende Pfad auf eine lokale Datei zeigt.
 aws cloudformation create-stack --stack-name WebSiteHosting-stack-tb --template-body file://C:\AWS-CloudFormation-Auto/statische-website-hosting.yaml
--- löschen-stack.ps1
+++ löschen-stack.ps1
@@ -1,10 +1,5 @@
-# AWS-Anmeldeinformationen konfigurieren
-#Set-AWSCredential -AccessKey 'ASIATGLVDFWDO6S5DN5K' -SecretKey 'f8P/XVvMB1rNYNnvcBOPeK9dcszKZRbqkTmfFRSB' -StoreAs 'MyProfile'
-
 # S3-Bucket löschen
-#aws s3 rm s3://trg-bucket-tb --recursive --profile MyProfile
-aws s3 rm s3://trg-bucket-tb --recursive
+aws s3 rm s3://trg-bucket-tb --recursive #--recursive ist erforderlich, wenn der S3-Bucket Unterverzeichnisse oder viele Objekte enthält, um den gesamten Inhalt des Buckets zu löschen.
 
 #CloudFormation-Stack löschen
-#aws cloudformation delete-stack --stack-name WebSiteHosting-stack-tb --profile MyProfile
 aws cloudformation delete-stack --stack-name WebSiteHosting-stack-tb
