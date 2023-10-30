ff23949a13030546f0004e12174391db56c40bfd
tidiane <tidian.balde@inform-software.com>
2023-10-30 15:41:18 +0100

    sleep funkt

--- aws-cloudformation-stack-erstellen.ps1
+++ aws-cloudformation-stack-erstellen.ps1
@@ -1,6 +1,9 @@
 #Das Präfix "file://" ist eine Konvention, um anzuzeigen, dass der nachfolgende Pfad auf eine lokale Datei zeigt.
 aws cloudformation create-stack --stack-name WebSiteHosting-stack-tb --template-body file://C:\AWS-CloudFormation-Auto/statische-website-hosting.yaml
 
+# Warten Sie 60 Sekunden
+sleep 60
+
 aws s3 cp C:\\AWS-CloudFormation-Auto\index.html s3://trg-bucket-tb/
 aws s3 cp C:\\AWS-CloudFormation-Auto\error.html s3://trg-bucket-tb/
 aws s3 cp C:\\AWS-CloudFormation-Auto\aws-logo.jpg s3://trg-bucket-tb/
