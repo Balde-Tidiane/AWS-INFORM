c5dfb01050ddbc3c81e2bd1b3b00d07cecde901f
tidiane <tidian.balde@inform-software.com>
2023-10-30 15:51:44 +0100

    a

--- aws-cloudformation-stack-erstellen.ps1
+++ aws-cloudformation-stack-erstellen.ps1
@@ -1,7 +1,8 @@
 #Das Präfix "file://" ist eine Konvention, um anzuzeigen, dass der nachfolgende Pfad auf eine lokale Datei zeigt.
 aws cloudformation create-stack --stack-name WebSiteHosting-stack-tb --template-body file://C:\AWS-CloudFormation-Auto/statische-website-hosting.yaml
 
-Write-Host "warten sie während der Statck noch erstellt wird!"
+Write-Host "Warten Sie ein bisschen. Der Stack wird nocht erstellt!"
+Write-Host ""
 # Warten Sie 60 Sekunden
 sleep 60
 
